faaa31b2665a3c3dcfefa0abd12b929553973fd8
Akash-Macha <akashmacha1999@gmail.com>
2024-01-28 01:01:20 +0530

    - Implemented /loaded-pdfs API - Added existing pickles

--- back-end/main.py
+++ back-end/main.py
@@ -1,15 +1,40 @@
+import os
 from fastapi import FastAPI
+from pydantic import BaseModel
+from dotenv import load_dotenv
+
+
+class Question(BaseModel):
+    question: str
+
+# Initialize the environment variables
+load_dotenv()
 
 app = FastAPI()
 
-@app.get('/')
-def hello():
-    return {"Hello": "World"}
+# APIs
 
-@app.post('/')
-def hello_post():
-    return {"Success": "You posted!"}
+@app.get('/loaded-pdfs')
+def get_loaded_pdfs():
+    all_file_names = os.listdir()
+    pickles = [x for x in all_file_names if x[-4:] == '.pkl']
+    loaded_pdf_names = list(map(lambda a: a[:-4], pickles)) # trimming the .pkl from the file name
 
-@app.get('/something')
-def something():
-    return {"Data": "Something"}
+    return {
+        "loaded_pdfs": loaded_pdf_names,
+        "total_count": len(pickles)
+    }
+
+# @app.post('/query')
+# def query(question: Question):
+#     print("[query]: " + question.question)
+#     return {"Data": question.question}
+
+# @app.post('/')
+# def hello_post():
+#     return {"Success": "You posted!"}
+
+# @app.get('/something')
+# def something():
+#     return {"Data": "Something"}
+
